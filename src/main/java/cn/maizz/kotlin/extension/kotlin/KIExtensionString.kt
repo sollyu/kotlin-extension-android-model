5374c2f1bdecf8dfcaf6930e49f37275975941ee
sollyu <sollyu@qq.com>
2018-12-19 15:07:31 +0800

    ●ˇ∀ˇ●

--- src/main/java/cn/maizz/kotlin/extension/kotlin/KIExtensionString.kt
+++ src/main/java/cn/maizz/kotlin/extension/kotlin/KIExtensionString.kt
@@ -45,7 +45,19 @@ interface KIExtensionString {
     /**
      * 判断是否保护中文
      */
-    fun String.isContainChinese():Boolean = this.contains(Pattern.compile("[\u4e00-\u9fa5]").toRegex())
+    fun String.isContainChinese(): Boolean = this.contains(Pattern.compile("[\u4e00-\u9fa5]").toRegex())
+
+    /**
+     * 判断是否一个邮箱
+     */
+    fun String.isEmailValid(): Boolean = Pattern.compile(
+        "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+    ).matcher(this).matches()
 
     /**
      * @param endIndex 结束位置
